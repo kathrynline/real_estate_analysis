75af6ee90922dbef47a948f5db2d9168f71bee0a
kathrynline <23664308+kathrynline@users.noreply.github.com>
2018-06-03 10:40:26 -0700

    first commit

---
+++ analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 19:44:47 2018
+
+@author: emilylinebarger
+"""
+import sklearn 
+import pandas as pd 
+df = pd.read_csv('/Users/emilylinebarger/Downloads/RDC_Inventory_current_State_price_100_200k.csv') 
